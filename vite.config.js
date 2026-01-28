dc21b56e872e13bb0e8f6db79a921de7e71d5f8a
zaid <mohdzaidshaikh4026@gmail.com>
2026-01-29 00:53:53 +0530

    adding chages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
     server: {
         host: true,
         port: 5173,
-        allowedHosts: ["cicd.mzsk.fun"],
+        allowedHosts: ["cicd-dev.mzsk.fun", "cicd-main.mzsk.fun"],
     },
 });
